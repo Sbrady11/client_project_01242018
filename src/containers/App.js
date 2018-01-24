afa3ab3de506cc7f017eff91f5c690d16e0413f7
Simon Brady <simonbrady@mac.com>
2018-01-24 11:38:07 -0500

    Set up reactstrap and bootstrap css

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -8,6 +9,9 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div>
+          <Button> hello </Button>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
