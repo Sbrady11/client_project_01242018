7331ecd2c25caae0943d5a9c9929c7a6f506287c
Simon Brady <simonbrady@mac.com>
2018-02-01 10:28:37 -0500

    Build components, basic file structure

---
+++ src/components/criteriaSelection.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+class Criteria extends React.Component {
+  render() {
+    return (
+      <div>
+        Criteria
+        <div className='page'>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Criteria;
---
+++ src/components/messageComp.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+class MessageComp extends React.Component {
+  render() {
+    return (
+      <div>
+        MessageComp
+        <div className='page'>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MessageComp;
