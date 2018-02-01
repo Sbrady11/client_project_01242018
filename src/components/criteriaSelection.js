543e7f9ed98ad53b1ee5914fe95f86b2a26e76cf
Simon Brady <simonbrady@mac.com>
2018-02-01 10:36:00 -0500

    Functioning selectors

--- src/components/criteriaSelection.js
+++ src/components/criteriaSelection.js
@@ -1,13 +1,36 @@
-
 import React from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
+
 
 class Criteria extends React.Component {
+    constructor (props) {
+    super(props);
+
+    this.state = { cSelected: [] };
+
+    this.onCheckboxBtnClick = this.onCheckboxBtnClick.bind(this);
+  }
+
+  onCheckboxBtnClick(selected) {
+    const index = this.state.cSelected.indexOf(selected);
+    if (index < 0) {
+      this.state.cSelected.push(selected);
+    } else {
+      this.state.cSelected.splice(index, 1);
+    }
+    this.setState({ cSelected: [...this.state.cSelected] });
+  }
   render() {
     return (
       <div>
         Criteria
-        <div className='page'>
-        </div>
+        <h5>Checkbox Buttons</h5>
+          <ButtonGroup>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(1)} active={this.state.cSelected.includes(1)}>One</Button>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(2)} active={this.state.cSelected.includes(2)}>Two</Button>
+            <Button color="primary" onClick={() => this.onCheckboxBtnClick(3)} active={this.state.cSelected.includes(3)}>Three</Button>
+          </ButtonGroup>
+        <p>Selected: {JSON.stringify(this.state.cSelected)}</p>
       </div>
     )
   }
--- src/components/messageComp.js
+++ src/components/messageComp.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 class MessageComp extends React.Component {
